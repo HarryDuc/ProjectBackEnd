63650f0e3d91bdf7e2f3ee06d87f81e8926a9ada
HarryDuc <vuminhduc.contact@gmail.com>
2025-02-03 10:17:07 +0700

    Update Auth-model

--- models/auth-model.js
+++ models/auth-model.js
@@ -24,101 +24,6 @@ const userSchema = mongoose.Schema({
 });
 const User = mongoose.model("user", userSchema);
 
-// exports.registerNewUser = (username, email, password) => {
-//   return new Promise((resolve, reject) => {
-//     mongoose.connect(DB_URL).then(() => {
-//       User.findOne({ email: email }).then((user) => {
-//         if (user) {
-//           reject("Tài khoản email đã được đăng ký.");
-//           mongoose.disconnect();
-//         } else {
-//           bcrypt.hash(password, 10).then((encryptedPassword) => {
-//             let newUser = new User({
-//               username: username,
-//               email: email,
-//               password: encryptedPassword,
-//             });
-//             newUser
-//               .save()
-//               .then(() => {
-//                 resolve();
-//                 mongoose.disconnect();
-//               })
-//               .catch((err) => {
-//                 reject(err);
-//                 mongoose.disconnect();
-//               });
-//           });
-//         }
-//       })
-//       .then(() => {
-//         return transporter.sendMail({
-//           from: '"Shop Online" <vuminhduc.contact@gmail.com>',
-//           to: email,
-//           subject: "Đăng ký tài khoản thành công!",
-//           text: "Cảm ơn bạn đã đăng ký tài khoản.",
-//           html: "<b>Chào mừng bạn!</b><p>Cảm ơn bạn đã đăng ký tài khoản với chúng tôi.</p>",
-//         });
-//       })
-//       .then((info) => {
-//         console.log("Email sent: %s", info.messageId);
-//         mongoose.disconnect();
-//         resolve("Tài khoản đã được tạo thành công.");
-//       })
-//       .catch((err) => {
-//         mongoose.disconnect();
-//         reject(err);
-//       });
-//     });
-//   });
-// };
-
-// exports.validateLogin = (email, password) => {
-//   return new Promise((resolve, reject) => {
-//     mongoose.connect(DB_URL).then(() => {
-//       User.findOne({ email: email })
-//         .then((user) => {
-//           if (!user) {
-//             reject("Không có tài khoản nào được tìm thấy.");
-//             mongoose.disconnect();
-//           } else {
-//             bcrypt.compare(password, user.password).then((same) => {
-//               if (!same) {
-//                 reject("Mật khẩu không chính xác!");
-//                 mongoose.disconnect();
-//               } else {
-//                 mongoose.disconnect();
-//                 resolve({
-//                   id: user._id,
-//                   isAdmin: user.isAdmin,
-//                 });
-//               }
-//             });
-//           }
-//         })
-//         .then(() => {
-//           return transporter.sendMail({
-//             from: '"Shop Online" <vuminhduc.contact@gmail.com>',
-//             to: email,
-//             subject: "Chào mừng bạn đến với Shop!",
-//             text: "Chào mừng bạn đã trở lại trang của chúng tôi.",
-//             html: "<b>Chào mừng bạn!</b><p>Cảm ơn bạn đã trở lại với chúng tôi.</p>",
-//           });
-//         })
-//         .then((info) => {
-//           console.log("Email sent: %s", info.messageId);
-//           mongoose.disconnect();
-//           resolve("Đăng nhập thành công.");
-//         })
-//         .catch((err) => {
-//           mongoose.disconnect();
-//           reject(err);
-//         });
-//     });
-//   });
-// };
-// exports.UserModel = User;
-
 exports.registerNewUser = (username, email, password) => {
   return new Promise((resolve, reject) => {
     mongoose.connect(DB_URL).then(() => {
@@ -146,10 +51,10 @@ exports.registerNewUser = (username, email, password) => {
                     html: "<b>Chào mừng bạn!</b><p>Cảm ơn bạn đã đăng ký tài khoản với chúng tôi.</p>",
                   })
                   .then((info) => {
-                    console.log("Email sent: %s", info.messageId);
+                    console.log("Gửi email: %s", info.messageId);
                   })
                   .catch((err) => {
-                    console.error("Failed to send email:", err);
+                    console.error("Gửi email thất bại:", err);
                   })
                   .finally(() => {
                     mongoose.disconnect();
@@ -198,10 +103,10 @@ exports.validateLogin = (email, password) => {
                   html: "<b>Chào mừng bạn!</b><p>Cảm ơn bạn đã trở lại với chúng tôi.</p>",
                 })
                   .then((info) => {
-                    console.log("Email sent: %s", info.messageId);
+                    console.log("Gửi email: %s", info.messageId);
                   })
                   .catch((err) => {
-                    console.error("Failed to send email:", err);
+                    console.error("Gửi email thất bại:", err);
                   })
                   .finally(() => {
                     mongoose.disconnect();
